83bb84ea5590ad3fb9ea24392ebcf3d4828d3175
JV <jv@off.lu>
2013-03-17 00:03:56 +0200

    Create csrf_test.go

---
+++ csrf_test.go
@@ -0,0 +1,45 @@
+package csrf
+
+import (
+  "encoding/base64"
+	"testing"
+)
+
+var (
+	action = string("POST /form")
+	id     = string("13243434")
+)
+
+func TestToken(t *testing.T) {
+	Key = Rand16()
+	s := newToken(action, id)
+	if !Valid(s, action, id) {
+		t.Error("Expected token to be valid.")
+	}
+	action = string("POST /diferentform")
+	if Valid(s, action, id) {
+		t.Error("Expected token to be invalid diferen actionID")
+	}
+	id = string("9894848493")
+	if Valid(s, action, id) {
+		t.Error("Expected token to be invalid diferen user ID")
+	}
+	d := newToken(action, id)
+	Key = Rand16()
+	if Valid(d, action, id) {
+		t.Error("Expected token to be invalid diferen hmac key")
+	}
+
+}
+
+// Test malformed
+func TestMalformed(t *testing.T) {
+	a := string(rand16())
+	if Valid(a, action, id) {
+		t.Errorf("Expected generated token to be invalid, malformed data")
+	}
+	b := base64.URLEncoding.EncodeToString([]byte("foobarzap1234567804343434"))
+	if Valid(b, action, id) {
+		t.Errorf("Expected generated token to be invalid, malformed data")
+	}
+}
